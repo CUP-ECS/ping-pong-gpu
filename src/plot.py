3df2842a932571706bf6b9f8413a232c209a4d10
Keira Haskins <keirahaskins26@gmail.com>
2021-07-26 11:44:20 -0600

    Updated presentation notes.

--- src/plot.py
+++ src/plot.py
@@ -12,27 +12,29 @@ import os
 ===============================================================================
 '''
 def gather_data():
-    df = pd.read_csv("ping_pong_direct.dat")
+    #df = pd.read_csv("ping_pong_direct.dat")
+    #df = pd.read_csv("ping_pong_cuda.dat")
+    df = pd.read_csv("ping_pong_copy.dat")
     #df = pd.read_csv("ping_pong.dat")
-    #ax = df.plot.gca()
-    # df.plot(kind='bar', rot=0, subplots=True)
-    axes = df.plot.line(subplots=True, x='max_i') 
+    axes = df.plot.line(subplots=True, x='max_i', marker='o') 
     axes[0].set_ylabel('seconds')
     axes[1].set_ylabel('seconds')
     axes[2].set_ylabel('GiB/s')
 
-    df.plot(kind='bar', rot=0, subplots=True, marker='o', linestyle='dashed')
+    #df.plot(kind='bar', rot=0, subplots=True, marker='o', linestyle='dashed')
     # set the spacing between subplots
-    plt.subplots_adjust(left=0.1,
-                        bottom=0.1, 
-                        right=0.9, 
-                        top=0.9, 
-                        wspace=0.4, 
-                        hspace=0.4)
+    #plt.subplots_adjust(left=0.1,
+    #                    bottom=0.1, 
+    #                    right=0.9, 
+    #                    top=0.9, 
+    #                    wspace=0.4, 
+    #                    hspace=0.4)
     #axes[1].legend(loc=2)
     type(axes)
     #plt.savefig('output.png')
-    plt.savefig('output_direct.png')
+    #plt.savefig('output_direct.png')
+    #plt.savefig('output_cuda.png')
+    plt.savefig('output_copy.png')
     #print(data)
 
 
