1bd443e3886e3e32a91a8cd467f71be9f2230357
Keira Haskins <keirahaskins26@gmail.com>
2021-08-19 15:06:49 -0600

    Added wrap lines to CMakeLists and removed data from data files.

--- src/plot.py
+++ src/plot.py
@@ -12,8 +12,8 @@ import sys
 @returns:
 ===============================================================================
 '''
-def gather_data( mode ):
-    #df = pd.DataFrame()
+def gather_data( mode, direction ):
+    df = pd.DataFrame()
     mode_str = ''
     if ( mode == '0' ):
         mode_str = 'direct'
@@ -25,7 +25,7 @@ def gather_data( mode ):
         print( 'Invalid mode chosen.' )
         return 0
     
-    df = pd.read_csv( 'ping_pong_' + mode_str + '.dat' ) 
+    df = pd.read_csv( 'ping_pong_' + mode_str + direction + '.dat' ) 
     #if ( mode == "0" ):
     #    df = pd.read_csv("ping_pong_direct.dat")
     #elif ( mode == "1" ): 
@@ -35,23 +35,31 @@ def gather_data( mode ):
     #else:
     #    print( "Invalid mode chosen." )
     #    return 0
+
+    colors = ['r','b','tab:orange']
     
     #df = pd.read_csv("ping_pong.dat")
-    axes = df.plot.line(subplots=True, x='max_i', marker='o') 
-    #axes[0].set_ylabel('seconds')
-    #axes[1].set_ylabel('seconds')
-    #axes[2].set_ylabel('GiB/s')
+    axes = df.plot.line( subplots=True, x='max_i', marker='o' )
+    #axes[0].set_ylabel( 'seconds' )
+    #axes[1].set_ylabel( 'seconds' )
+    #axes[2].set_ylabel(   'MiB/s' )
 
-    axes[0].set_ylabel('seconds')
-    axes[1].set_ylabel('GiB/s')
+    #axes[0].set_color('r')
+    #axes[1].set_color('b')
+    #axes[2].set_color('o')
 
-    #axes[0].set(ylim=(0,30))
-    #axes[1].set(ylim=(0.00,0.02))
-    #axes[2].set(ylim=(0,1.5e10))
+    axes[0].set_ylabel( 'seconds' )
+    axes[1].set_ylabel(   'GiB/s' )
 
-    #axes[0].set(ylim=(0,30))
-    axes[0].set(ylim=(0.00,0.02))
+    #axes[0].set(ylim=(0,3000))
+    #axes[1].set(ylim=(0.00,2))
+    #axes[2].set(ylim=(0,0.05e8))
+
+    axes[0].set(ylim=(0.00,2))
     axes[1].set(ylim=(0,1.5e10))
+
+    #axes[0].set(ylim=(0.00,0.08))
+    #axes[1].set(ylim=(0,1.5e10))
 
     #df.plot(kind='bar', rot=0, subplots=True, marker='o', linestyle='dashed')
     # set the spacing between subplots
@@ -63,7 +71,7 @@ def gather_data( mode ):
     #                    hspace=0.4)
     #axes[1].legend(loc=2)
     type(axes)
-    plt.savefig( 'output_' + mode_str + '_new.png' )
+    plt.savefig( 'output_' + mode_str + direction + '_new.png' )
     #if ( mode == "0" ):
     #    plt.savefig('output_direct_new.png')
     #elif ( mode == "1" ): 
@@ -75,7 +83,7 @@ def gather_data( mode ):
     #print(data)
 
 def main( argv ):
-    gather_data( argv[1] )
+    gather_data( argv[1], argv[2] )
 
 '''
 ===============================================================================
