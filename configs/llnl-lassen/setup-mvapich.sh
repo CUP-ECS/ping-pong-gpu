468f76c3a42fddc2b501a7af38f4d76cc2f5c9fd
Patrick G. Bridges <patrickb@unm.edu>
2022-10-27 15:07:16 -0700

    Created spack and shell config files for use on LLNL lassen

---
+++ configs/llnl-lassen/setup-mvapich.sh
@@ -0,0 +1,3 @@
+module load gcc/7.3.1
+module load mvapich2/2021.05.28-cuda-11.6.1
+spack env activate $(HOME)/ping-pong-gpu/build-lassen-mvapich
---
+++ configs/llnl-lassen/setup-spectrum.sh
@@ -0,0 +1,2 @@
+module load gcc/7.3.1
+spack env activate $(HOME)/ping-pong-gpu/build-lassen-spectrum
